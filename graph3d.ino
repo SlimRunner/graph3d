006f72181043ecb28d489ba57acaf48ae0e8308c
SlimRunner <dv.polygon@gmail.com>
2023-07-22 00:02:15 -0700

    setup screen

--- graph3d.ino
+++ graph3d.ino
@@ -6,8 +6,18 @@
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_ADDR 0x3C
 
+// declare an SSD1306 display object connected to I2C
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
 void setup() {
-  
+  Serial.begin(115200);
+
+  // initialize OLED display with address 0x3C for 128x64
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    while (true);
+  }
+  oled.display();
 }
 
 void loop() {
