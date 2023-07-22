8f84f62a4a98670da94ad1c85519506ad7fe349d
SlimRunner <dv.polygon@gmail.com>
2023-07-21 23:59:57 -0700

    initial commit

---
+++ graph3d.ino
@@ -0,0 +1,15 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width,  in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_ADDR 0x3C
+
+void setup() {
+  
+}
+
+void loop() {
+  
+}
